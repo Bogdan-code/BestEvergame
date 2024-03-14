b22e0f8075ef6ee207ebd0ccb2285d30e295e0aa
DJKhelb <frassesmail@gmail.com>
2024-03-14 21:03:29 +0100

    fuck aina

---
+++ nigersinParis/Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,25 @@
+using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x * Mathf.Rad2Deg);
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+
+    }
+}
