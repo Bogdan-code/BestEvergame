6698672fda94f296534811203e2b45045c52f178
DjDalyDalito <161302732+DjDalyDalito@users.noreply.github.com>
2024-03-21 20:37:06 +0100

    1

--- nigersinParis/Assets/Scripts/Shooting.cs
+++ nigersinParis/Assets/Scripts/Shooting.cs
@@ -9,12 +9,11 @@ public class Shooting : MonoBehaviour
     private Camera mainCam;
     private Vector3 mousePos;
     public GameObject bullet;
+    private GameObject currentBullet;
     public Transform bulletTransform;
-    public bool canFire;
-    private float timer;
-    public float timeBetweenFiring;
-
-    public Transform aimTransform;
+    public bool isFiring;
+    public float firingDuration = 5f;
+    private Transform aimTransform;
     
     // Start is called before the first frame update
     void Start()
@@ -26,44 +25,44 @@ public class Shooting : MonoBehaviour
     void Update()
     {
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
 
         Vector3 rotation = mousePos - transform.position;
-
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
 
-        if (!canFire)
+        if (Input.GetMouseButtonDown(0))
         {
-            timer += Time.deltaTime;
-            if(timer > timeBetweenFiring)
-            {
-                canFire = true;
-                timer = 0;
-            }
+            StartFiring();
         }
 
-        if (Input.GetMouseButton(0) && canFire)
+        if(Input.GetMouseButtonUp(0))
         {
-            canFire = false;
-            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+            StopFiring(); 
         }
-        
-        
-        
         
         Vector3 aimlocalScale = Vector3.one;
-        if ( rotZ > 90 || rotZ < -90)
-        {
-            aimlocalScale.y = -1f;
-        }
-        else
-        {
-            aimlocalScale.y = +1f;
-        }
+        aimlocalScale.y = rotZ > 90 || rotZ < -90 ? -1f : 1f;
         aimTransform.localScale = aimlocalScale;
+
     }
 
-    
+    void StartFiring()
+    {
+        if (!isFiring)
+        {
+            isFiring = true;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
+    }
+
+    void StopFiring()
+    {
+        if (isFiring)
+        {
+            isFiring = false;
+            Destroy(currentBullet, firingDuration);
+        }
+    }
 
   }
